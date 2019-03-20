88ee91d2b56a8cdfd2c313ef0ae5ef0f62208a54
olympia <owojcik7@gmail.com>
2019-03-20 18:58:50 -0400

    Add ability to GET all posts by specific user id

--- data/users/users-router.js
+++ data/users/users-router.js
@@ -15,3 +15,22 @@ router.get("/", async (req, res) => {
 });
 
 module.exports = router;
+
+router.get("/:id", async (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
+  try {
+    const userPosts = await db.getUserPosts(req.params.id);
+    console.log(userPosts);
+    if (userPosts.length) {
+      res.status(200).json(userPosts);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The specified user does not have any posts" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "The user's posts could not be retrieved" });
+  }
+});
