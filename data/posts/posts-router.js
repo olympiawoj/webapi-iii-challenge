f7cbcd82d0007b71ca20744ea8dff273e6bec2f1
olympia <owojcik7@gmail.com>
2019-03-20 17:32:41 -0400

    Add ability to get all posts and get post by id

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -14,4 +14,27 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
+  const { id } = req.params;
+
+  try {
+    const post = await db.getById(id);
+    console.log(post);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified id does not exist" });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved" });
+  }
+});
+
 module.exports = router;
