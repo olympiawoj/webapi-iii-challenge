75353c782b412905e2a859636694e6d2a6b8bbba
olympia <owojcik7@gmail.com>
2019-03-20 17:51:45 -0400

    Add ability to POST & insert new post to db

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -37,4 +37,25 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
+  try {
+    const post = await db.insert(req.body);
+    res.status(201).json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error adding the post" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
