928e048f8f4797f4e644351907587c342b63dc03
olympia <owojcik7@gmail.com>
2019-03-21 16:12:37 -0400

    Add changes for heroku deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //This file index.js is in charge of running the server and telling us which port API is listening on
 //We must bring in the server from server.js
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const server = require("./server.js");
 
