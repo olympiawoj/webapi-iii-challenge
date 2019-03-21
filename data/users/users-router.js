13e3d4e7a1d79a78b38116129491c879f40895cc
olympia <owojcik7@gmail.com>
2019-03-21 09:37:30 -0400

    Pushing latest code

--- data/users/users-router.js
+++ data/users/users-router.js
@@ -4,6 +4,7 @@ const db = require("./userDb.js");
 
 const router = express.Router();
 
+//Get a list of all users
 router.get("/", async (req, res) => {
   try {
     const users = await db.get();
@@ -14,7 +15,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+//Get a user with a specific user id
 router.get("/:id", async (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
+  try {
+    const user = await db.getById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ errorMessage: "Error getting user with specific id" });
+  }
+});
+
+//Get all posts by specific user ID
+router.get("/:id/posts", async (req, res) => {
   console.log(req.params);
   console.log(req.body);
   try {
@@ -33,6 +50,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//Add a new user to user list
 router.post("/", async (req, res) => {
   console.log(req.params);
   console.log(req.body);
@@ -49,6 +67,7 @@ router.post("/", async (req, res) => {
   }
 });
 
+//Delete user from user list
 router.delete("/:id", async (req, res) => {
   console.log(req.params);
   try {
@@ -62,6 +81,27 @@ router.delete("/:id", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ errorMessage: "Error removing this user" });
+  }
+});
+
+//Update user
+router.put(":/id", async (req, res) => {
+  console.log(req.body);
+  if (!req.body.name) {
+    res.status(404).json({ errorMessage: "Please provide name for user" });
+  } else {
+    try {
+      const user = await db.update(req.params.id, req.body);
+      console.log(user);
+      if (user.length) {
+        res.status(200).json({ user });
+      } else {
+        res.status(204).json({ message: "The user could not be found" });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ errorMessage: "Error updating this user" });
+    }
   }
 });
 
--- server.js
+++ server.js
@@ -3,11 +3,25 @@
 
 const express = require("express");
 const server = express();
+
+//Router imports
 const postsRouter = require("./data/posts/posts-router.js");
 const usersRouter = require("./data/users/users-router.js");
 
+// // Custom middleware, capitalize if string
+// function capUser(req, res, next) {
+//   console.log("You are using capUser ok");
+//   const name = req.body.name;
+//   return name.charAt(0).toUpperCase() + name.slice(1);
+//   next(); //calling next signals that the middleware has finished and should call the next middleware function.
+// }
+
 server.use(express.json());
 
+// This would be global middleware
+// server.use(capUser);
+
+//routing
 server.use("/api/posts", postsRouter);
 server.use("/api/users", usersRouter);
 
